9c5b10d33abeb9b961f4c9adafce3399fb922832
Elena Leib <eleib@berkeley.edu>
2020-09-12 13:44:57 -0700

    Create Week4_practice_starter.R

---
+++ Week4_practice_starter.R
@@ -0,0 +1,75 @@
+# QuACK 2020 
+# Week 4 - Data Cleaning and Tidyverse
+# Practice
+# Authors: Willa & Elena
+
+#### Set options ####
+# Set your stringAsFactors option to false!
+# I always do this at the top of all of my scripts.
+
+
+#### 1. Check/Set your working directory ####
+# Make sure you are where you want to be.
+
+
+#### 2. Load your data ####
+# Data adapted from Allison Horst and her Palmer Penguins
+# https://github.com/allisonhorst/palmerpenguins
+# Load in penguin.csv
+
+
+
+#### 3. Check for missing values ####
+
+# i. Are there any missing values? (There are a few ways you can check for this.)
+
+
+
+# ii. Remove those penguins!
+
+
+
+
+##### 4. Get to know some of these penguins ##### 
+
+# i. Pull the all available info for penguin p5
+
+
+
+# ii. How much does penguin p18 weigh? 
+
+
+
+# iii. Who is heavier p18 or p38?
+
+
+
+
+#### 5. On average, do Adelie or Gentoo penguins have longer bill length? ####
+
+
+
+
+#### 6. Subsetting data ####
+# In the following exerices, subset penguins using both bracket notation and the subset function
+# i. Make a data frame of all the penguins that weigh more than p18
+
+
+
+# ii. Make new data frames, one for each island. 
+
+
+
+
+# iii. Make a new data frame of all the Adelie penguins on Dream island.
+
+
+
+# iv. Do the same as in iii, but now only select the species, island, bill_length_mm, and sex columns for your new data frame
+
+
+
+#### 7. Challenge: What island has the most Adelie species ####
+# Hint: Use table() function
+
+
