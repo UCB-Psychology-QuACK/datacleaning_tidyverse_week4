4db859bf03438baebe132927b31ef636e11ee55b
Elena Leib <eleib@berkeley.edu>
2020-09-13 11:37:09 -0700

    Create Week4_datacleaning_tidyverse_part2_key.R

---
+++ Week4_datacleaning_tidyverse_part2_key.R
@@ -0,0 +1,47 @@
+# QuACK 2020 
+# Week 4 - Data Cleaning and Tidyverse
+# Class Part 2
+# Authors: Elena & Willa
+
+#### Set options ####
+# Set your stringAsFactors option to false!
+# I always do this at the top of all of my scripts.
+
+
+#### NEW! Import packages ####
+# If this is your first time using a package, you will need to install it first!
+
+# Install packages
+# In the bottom right window, click on the 'Packages' tab. Then click 'Install' and type in the name of the package you want to install.
+# In this case, we want to install 'tidyverse' and 'tidylog'. 
+# Type in the package (it should autofill), and click install.
+
+# Alternatively, you can type:
+install.packages("tidyverse")
+install.packages("tidylog")
+
+# Import packages
+library(tidyverse)
+library(tidylog)
+
+
+#### 1. Check/Set your working directory ####
+# Make sure you are where you want to be.
+
+
+#### 2. Load your data ####
+# Data adapted from Allison Horst and her Palmer Penguins
+# https://github.com/allisonhorst/palmerpenguins
+# Load in penguin.csv
+
+
+# Look up what we did last year...!
+
+
+# Intro
+# - filter
+# - select
+# - mutate
+# - drop_na
+# - group_by
+# - summarise
